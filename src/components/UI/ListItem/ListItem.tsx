b6d650951d6eb7c2387f0ac3f6aa8e80c1958058
mahyarnafisi <mahyar.nafisi@gmail.com>
2024-02-08 13:40:11 +0100

    ..

--- src/components/UI/ListItem/ListItem.tsx
+++ src/components/UI/ListItem/ListItem.tsx
@@ -1,11 +1,14 @@
 import Styles from "./ListItem.module.css";
+import CloseOutlinedIcon from "@mui/icons-material/CloseOutlined";
 function ListItem() {
   return (
     <div className={Styles.listItem}>
-      <div className={Styles.colorTag}>color</div>
-      <h2 className={Styles.title}>title item</h2>
-      <h2 className={Styles.shop}>shop</h2>
-      <button className={Styles.btn__rmv}>close</button>
+      <div className={Styles.colorTag}></div>
+      <h2 className={Styles.title}>Title item to buy</h2>
+      <h2 className={Styles.shop}>lidl</h2>
+      <button className={Styles.btn__rmv}>
+        <CloseOutlinedIcon />
+      </button>
     </div>
   );
 }
